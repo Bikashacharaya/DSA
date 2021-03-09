ce5e21f1b5cf14bbd5dfabd3dc8e344fd48f4a3e
Bikash Acharaya <63426263+Bikashacharaya@users.noreply.github.com>
2021-03-09 17:15:12 +0530

    Add files via upload

---
+++ Assignmnet Day-1/Question1.py
@@ -0,0 +1,4 @@
+x=int(input("Enter the value of x: "))
+y=int(input("Enter the value of y: "))
+z=pow(x,y)
+print(f"The result of X^Y is: {z}")5
---
+++ Assignmnet Day-1/Question2.py
@@ -0,0 +1,8 @@
+lst=[]
+for i in range(1,51):
+    if i%3==0:
+        lst.append(i)
+print(f"The number in the list which are divisible by 3 between 1 to 50 are: {lst}")
+        
+
+
